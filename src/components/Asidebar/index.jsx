91de85fb9160816409210c2f9436d393ef82d11f
Joris Bertier <joris.bertier@gmail.com>
2024-09-09 10:43:01 -0400

    aside

---
+++ src/components/Asidebar/index.jsx
@@ -0,0 +1,13 @@
+
+
+function Asidebar() {
+    return (
+        <aside>
+            <div>
+                <img src={} alt=''/>
+            </div>
+        </aside>
+    )
+}
+
+export default Asidebar
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
+import Asidebar from "../../components/Asidebar"
+
 function Home() {
     return (
-        <div className="test">
+        <div className="home__wrapper">
+            <Asidebar/>
+            <div className="home__wrapper--section2">
+
             <h1>Voici la page principale</h1>
+            </div>
         </div>
     )
 }
