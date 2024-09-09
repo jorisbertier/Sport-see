c38968d8a0c7224c98fab5bf17ae1665dfefb3ca
Joris Bertier <joris.bertier@gmail.com>
2024-09-09 11:46:24 -0400

    add style home

--- src/components/Asidebar/index.jsx
+++ src/components/Asidebar/index.jsx
@@ -1,11 +1,18 @@
-
+import Yoga from '../../assets/images/yoga.png';
+import Bodyweight from '../../assets/images/bodyweight.png';
+import Swimming from '../../assets/images/swimming.png';
+import Bicycle from '../../assets/images/bicycle.png';
 
 function Asidebar() {
     return (
         <aside>
-            <div>
-                <img src={} alt=''/>
+            <div className='aside__wrapper'>
+                <img src={Yoga} alt=''/>
+                <img src={Swimming} alt=''/>
+                <img src={Bicycle} alt=''/>
+                <img src={Bodyweight} alt=''/>
             </div>
+            <small>Copyright SportSee 2020 ©</small>
         </aside>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,18 @@ import Asidebar from "../../components/Asidebar"
 
 function Home() {
     return (
-        <div className="home__wrapper">
+        <div className="home">
             <Asidebar/>
-            <div className="home__wrapper--section2">
-
-            <h1>Voici la page principale</h1>
+            <div className="home__wrapper">
+                <div className="home__wrapper--banner">
+                    <h1>Bonjour Thomas</h1>
+                    <span> Félicitation ! Vous avez explosé vos objectifs hier 👏 </span>
+                    
+                </div>
+                <div className="home__wrapper--statistics">
+                    <div className="home__chart"> Chart</div>
+                    <div className="home__statistics"> Statistsique</div>
+            </div>
             </div>
         </div>
     )
