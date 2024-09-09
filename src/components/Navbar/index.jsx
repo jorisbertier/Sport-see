cff35151dee0725bfc74794d3ebb98aab557f3dd
Joris Bertier <joris.bertier@gmail.com>
2024-09-09 10:24:59 -0400

    add font

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,20 @@
+import Logo from '../../assets/images/logo.png';
+
+function Navbar() {
+
+    return(
+        <nav>
+            <div className='nav__wrapper--img'>
+                <img src={Logo}/>
+            </div>
+            <ul className='nav__wrapper--navlink'>
+                <li>Accueil</li>
+                <li>Profil</li>
+                <li>Réglage</li>
+                <li>Communauté</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import '../src/assets/style/main.scss'
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar/>
     <Home />
   </React.StrictMode>
 );
