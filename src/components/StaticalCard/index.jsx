8b268be1f556d92cb6ae7c4f2ce3d5f3f9087e7a
Joris Bertier <joris.bertier@gmail.com>
2024-09-10 15:44:15 -0400

    add props and style component StaticalCard

--- src/components/StaticalCard/index.jsx
+++ src/components/StaticalCard/index.jsx
@@ -1,9 +1,9 @@
-function StaticalCard({dataNutriment, image, typeOfNutriment}) {
+function StaticalCard({dataNutriment, image, typeOfNutriment, unit}) {
     return (
         <div className="home__statistics__wrapper">
             <img className="home__statistics__wrapper--icon"src={image} alt="Icon bodyweight"/>
             <div className="home__statistics__wrapper--text">
-                <h3>{dataNutriment} kcal</h3>
+                <h3>{dataNutriment}{unit}</h3>
                 <p>{typeOfNutriment}</p>
             </div>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import calories from '../../assets/images/calories-icon.png';
 import carbs from '../../assets/images/carbs-icon.png';
 import fat from '../../assets/images/fat-icon.png';
 import { useState, useEffect } from "react";
+import StaticalCard from "../../components/StaticalCard";
 
 function Home() {
 
@@ -29,40 +30,14 @@ function Home() {
                 <div className="home__wrapper--banner">
                     <h1>Bonjour <span>{user?.userInfos.firstName}</span></h1>
                     <p> Félicitation ! Vous avez explosé vos objectifs hier 👏 </p>
-                    
                 </div>
                 <div className="home__wrapper--statistics">
                     <div className="home__chart"> Chart</div>
                     <div className="home__statistics"> 
-                        <div className="home__statistics__wrapper">
-                            <img className="home__statistics__wrapper--icon"src={calories} alt="Icon bodyweight"/>
-                            <div className="home__statistics__wrapper--text">
-                                <h3>{user?.keyData.calorieCount} kcal</h3>
-                                <p>Calories</p>
-                            </div>
-                        </div>
-                        <div className="home__statistics__wrapper">
-                            <img className="home__statistics__wrapper--icon"src={proteins} alt="Icon bodyweight"/>
-                            <div className="home__statistics__wrapper--text">
-                                <h3>{user?.keyData.proteinCount}g</h3>
-                                <p>Protéines</p>
-                            </div>
-                        </div>
-                        <div className="home__statistics__wrapper">
-                            <img className="home__statistics__wrapper--icon"src={carbs} alt="Icon bodyweight"/>
-                            <div className="home__statistics__wrapper--text">
-                                <h3>{user?.keyData.carbohydrateCount}g</h3>
-                                <p>Glucides</p>
-                            </div>
-                        </div>
-                        <div className="home__statistics__wrapper">
-                            <img className="home__statistics__wrapper--icon"src={fat} alt="Icon bodyweight"/>
-                            <div className="home__statistics__wrapper--text">
-                                <h3>{user?.keyData.lipidCount}g</h3>
-                                <p>Lipides</p>
-                            </div>
-                        </div>
-    
+                        <StaticalCard dataNutriment={user?.keyData.calorieCount} typeOfNutriment="Calories" image={calories} unit=" kcal"/>
+                        <StaticalCard dataNutriment={user?.keyData.proteinCount} typeOfNutriment="Protéines" image={proteins} unit="g"/>
+                        <StaticalCard dataNutriment={user?.keyData.carbohydrateCount} typeOfNutriment="Glucides" image={carbs} unit="g"/>
+                        <StaticalCard dataNutriment={user?.keyData.lipidCount} typeOfNutriment="Lipides" image={fat} unit="g"/>
                     </div>
             </div>
             </div>
