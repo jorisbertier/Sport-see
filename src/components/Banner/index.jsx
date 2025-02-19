c2352ec1ced779b653d2f6cf6847ba8b2ed682a5
Joris Bertier <joris.bertier@gmail.com>
2025-02-19 01:46:26 +0100

    comment

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,9 +2,9 @@ function Banner({name}) {
     return(
         <header className="home__wrapper--banner">
             <h1>Bonjour <span>{name}</span></h1>
-            <p> Félicitation ! Vous avez explosé vos objectifs hier 👏 </p>
+            <p> Félicitations ! Vous avez explosé vos objectifs hier 👏 </p>
         </header>
     )
 }
-
+//banner
 export default Banner
